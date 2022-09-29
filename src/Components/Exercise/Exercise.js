908db722c0f00b09a6d369e3ca3216a0cfbdcf48
unknown <afia.nasrin3e@gmail.com>
2022-09-29 17:29:37 +0600

    exercise component are compleate

--- src/Components/Exercise/Exercise.js
+++ src/Components/Exercise/Exercise.js
@@ -4,27 +4,23 @@ const Exercise = ({ exercise, handleAddToTime }) => {
   const { name, id, time, age, about, picture } = exercise;
 
   return (
-    <div className="exercise">
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <figure className="px-10 pt-10">
-          <img src={picture} alt="Shoes" className="rounded-xl" />
-        </figure>
-        <div className="card-body items-center text-center">
-          {/* <h2 className="card-title">Shoes!</h2> */}
-          <p>Name:{name}</p>
-          <p>Age:{age}</p>
-          <p>id:{id}</p>
-          <p>Time Required:{time}s</p>
-          <p>About:{about.slice(0, 30)}</p>
-          {/* <p>About:{about ? about?.slice(0, 20) : "N/A"}</p> */}
-          <div className="card-actions">
-            <button
-              onClick={() => handleAddToTime(exercise)}
-              className="btn btn-primary"
-            >
-              Add To Cart
-            </button>
-          </div>
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <figure className="px-10 pt-10">
+        <img src={picture} alt="Shoes" className="rounded-xl" />
+      </figure>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title text-2xl">Exercise Name:{name}</h2>
+        <p className="text-2xl pb-2">Time Required:{time}s</p>
+        <p className="text-xl pb-2">Age:{age}</p>
+        <p className="text-xl pb-2">About:{about.slice(0, 60)}.....</p>
+
+        <div className="card-actions">
+          <button
+            onClick={() => handleAddToTime(exercise)}
+            className="btn btn-primary"
+          >
+            Add To Cart Time
+          </button>
         </div>
       </div>
     </div>
